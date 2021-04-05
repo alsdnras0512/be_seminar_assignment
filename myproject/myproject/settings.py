16deb76c47be878eb8aea8dd992e716e8f5af732
alsdnras0512 <alsdnras0512@gmail.com>
2021-04-05 23:19:34 +0900

    assignment1

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "posts",
-    "requests_test",
-]
+    ]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
--- myproject/requests_test/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequestsTestConfig(AppConfig):
-    name = 'requests_test'
